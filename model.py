e937a1a856caeb78ab0561ab082261f856159a2b
Nnexxus <Nnexxus@gmail.com>
2018-11-06 22:27:40 +0100

    WIP maneuvers

--- model.py
+++ model.py
@@ -1,13 +1,21 @@
+from datetime import datetime, timezone
+
 import orekit
 import numpy as np
 import pandas as pd
 
-from orekit.pyhelpers import setup_orekit_curdir, absolutedate_to_datetime
+from orekit.pyhelpers import setup_orekit_curdir, absolutedate_to_datetime, datetime_to_absolutedate
 from org.orekit.propagation.analytical.tle import TLE, TLEPropagator
 from org.orekit.frames import FramesFactory
 from org.orekit.bodies import OneAxisEllipsoid
 from org.orekit.utils import IERSConventions, Constants
 from org.orekit.orbits import KeplerianOrbit
+from org.orekit.forces.maneuvers import SmallManeuverAnalyticalModel
+from org.orekit.propagation.events import DateDetector
+from org.orekit.attitudes import LofOffset
+from org.orekit.frames import LOFType
+from org.hipparchus.geometry.euclidean.threed import Vector3D
+from org.orekit.propagation.analytical import AdapterPropagator
 
 print("Initializing Orekit")
 orekit.initVM()
@@ -15,9 +23,16 @@ setup_orekit_curdir()
 INERTIAL_FRAME = FramesFactory.getEME2000()
 
 
-def propagate(tle_line1, tle_line2, duration, step):
+def propagate(tle_line1, tle_line2, duration, step, maneuvers=[]):
     tle = create_tle(tle_line1, tle_line2)
-    propagator = TLEPropagator.selectExtrapolator(tle)
+    tle_propagator = TLEPropagator.selectExtrapolator(tle)
+    propagator_with_maneuvers = AdapterPropagator(tle_propagator)
+
+    # TODO works only for chronological maneuvers, because of the "propagate" to get the state
+    for maneuver in maneuvers:
+        date, frame, deltaV, isp = maneuver
+        propagator_with_maneuvers.addEffect(SmallManeuverAnalyticalModel(propagator_with_maneuvers.propagate(datetime_to_absolutedate(date)),
+                                                                         frame, deltaV, isp))
 
     ITRF = FramesFactory.getITRF(IERSConventions.IERS_2010, True)
     earth = OneAxisEllipsoid(Constants.WGS84_EARTH_EQUATORIAL_RADIUS,
@@ -26,12 +41,11 @@ def propagate(tle_line1, tle_line2, duration, step):
 
     time = [tle.getDate().shiftedBy(float(dt)) \
             for dt in np.arange(0, duration, step)]
-    orbits = [KeplerianOrbit(propagator.getPVCoordinates(date), propagator.getFrame(), date, Constants.EIGEN5C_EARTH_MU)
-              for date in time]
+    orbits = [KeplerianOrbit(propagator_with_maneuvers.propagate(date).getOrbit()) for date in time]
 
-    subpoints = [earth.transform(propagator.getPVCoordinates(date), propagator.getFrame(), date)
-                 for date in time]
-
+    subpoints = [
+        earth.transform(propagator_with_maneuvers.propagate(date).getPVCoordinates(), tle_propagator.getFrame(), date)
+        for date in time]
     return pd.DataFrame({'time': [absolutedate_to_datetime(orbit.getDate()) for orbit in orbits],
                          'a': [orbit.getA() for orbit in orbits],
                          'e': [orbit.getE() for orbit in orbits],
@@ -55,4 +69,13 @@ if __name__ == "__main__":
     spot5_1 = " 1 27421U 02021A   02124.48976499 -.00021470  00000-0 -89879-2 0    20"
     spot5_2 = " 2 27421  98.7490 199.5121 0001333 133.9522 226.1918 14.26113993    62"
 
-    propagate(spot5_1, spot5_2, 5 * 86400, 60)
+    maneuvers = [(datetime(2002, month=5, day=5, hour=12, minute=0, second=0, microsecond=0, tzinfo=timezone.utc),
+                  FramesFactory.getEME2000(), Vector3D(100., 0., 0.), 300.),
+                 (datetime(2002, month=5, day=6, hour=12, minute=0, second=0, microsecond=0, tzinfo=timezone.utc),
+                  FramesFactory.getEME2000(), Vector3D(0., 100., 0.), 300.),
+                 (datetime(2002, month=5, day=7, hour=12, minute=0, second=0, microsecond=0, tzinfo=timezone.utc),
+                  FramesFactory.getEME2000(), Vector3D(0., 0., 100.), 300.)
+                 ]
+
+    ephemeris = propagate(spot5_1, spot5_2, 5 * 86400, 60, maneuvers)
+    print(ephemeris)
--- view.py
+++ view.py
@@ -1,26 +1,45 @@
+from datetime import timezone, datetime
+
 from bokeh.layouts import widgetbox, layout
 
 from model import propagate
 from bokeh.server.server import Server
 from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, TextInput, Button
+from bokeh.models import ColumnDataSource, TextInput, Button, DataTable, TableColumn
 
 
 def compute(source, tle1, tle2):
     print("Computing...")
     source.data = ColumnDataSource(propagate(tle1, tle2, 5 * 86400, 60)).data
 
+
 def create_figure(x, y, source, title):
-    fig = figure(plot_width=400, plot_height=400, title = title, toolbar_location="below")
+    fig = figure(plot_width=400, plot_height=400, title=title, toolbar_location="below")
     fig.line(x=x, y=y, source=source)
     return fig
-
 
 
 def modify_doc(doc):
     satellite = TextInput(title="Satellite:", value="TELSTAR 12V")
     tle1 = TextInput(title="TLE line 1:", value="1 41036U 15068A   18304.93002776 -.00000131  00000-0  00000-0 0  9990")
     tle2 = TextInput(title="TLE line 2:", value="2 41036   0.0170 254.0407 0001996 337.8139 128.1274  1.00270199 10707")
+
+    maneuvers = ColumnDataSource(data=
+                                 {"date": [datetime(2002, month=5, day=5, hour=12, minute=0, second=0, microsecond=0,
+                                                    tzinfo=timezone.utc),
+                                           datetime(2002, month=5, day=6, hour=12, minute=0, second=0, microsecond=0,
+                                                    tzinfo=timezone.utc),
+                                           datetime(2002, month=5, day=7, hour=12, minute=0, second=0, microsecond=0,
+                                                    tzinfo=timezone.utc)],
+                                  "deltaV_X": [100, 0, 0],
+                                  "deltaV_Y": [0, 100, 0],
+                                  "deltaV_Z": [0, 0, 100],
+                                  "isp": [300, 300, 300]})
+    maneuvers_table = DataTable(source=maneuvers, columns=[TableColumn(field="date", title="date"),
+                                                           TableColumn(field="deltaV_X", title="deltaV_X"),
+                                                           TableColumn(field="deltaV_Y", title="deltaV_Y"),
+                                                           TableColumn(field="deltaV_Z", title="deltaV_Z")],
+                                editable=True)
 
     ephemeris = ColumnDataSource()
 
@@ -34,7 +53,7 @@ def modify_doc(doc):
 
     recompute_button = Button(label="Recompute")
     recompute_button.on_click(recompute)
-    doc.add_root(layout([[widgetbox(satellite, tle1, tle2, recompute_button)],
+    doc.add_root(layout([[widgetbox(satellite, tle1, tle2, recompute_button), widgetbox(maneuvers_table)],
                          [fig_long_sma, fig_ex_ey, fig_hx_hy]]))
 
 
